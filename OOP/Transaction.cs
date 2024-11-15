bacd3113b20c434313fcb5d95ca85bacca7a40b7
Duswcfamm <111428474+Duswcfamm@users.noreply.github.com>
2024-11-15 15:51:31 +0700

    Update Transaction.cs

--- OOP/Transaction.cs
+++ OOP/Transaction.cs
@@ -7,5 +7,7 @@ namespace PracticeOOP
         public int ID { get; set; }
         public DateTime createdAt { get; set; }
         public double totalAmount { get; set; }
+
+        public abstract void SaveTransaction();
     }
 }
